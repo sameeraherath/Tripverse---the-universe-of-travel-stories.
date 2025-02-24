edbb0abecd5fa2a6e104a15fc4a85e264bfec9c0
sameeraherath <sameerastar6@gmail.com>
2025-02-24 07:24:05 +0530

    Fix auth bug

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -10,6 +10,7 @@ const CreatePost = () => {
       await axios.post(`${import.meta.env.VITE_API_URL}/api/posts`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
+          withCredentials: true,
         },
       });
       navigate("/home");
