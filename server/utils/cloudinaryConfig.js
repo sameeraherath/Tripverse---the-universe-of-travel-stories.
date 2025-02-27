9213db2cd94ec9a2d9fea541d7d2635851d744e6
sameeraherath <sameerastar6@gmail.com>
2025-02-27 20:30:34 +0530

    Improve code readability

--- server/utils/cloudinaryConfig.js
+++ server/utils/cloudinaryConfig.js
@@ -6,9 +6,9 @@ const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: "dxugdxrqx",
-  api_key: "681498867918831", // Your API key
-  api_secret: "f6eDjwImqYHPSfprI93HS7B3R4I", // Your API secret
+  api_key: "681498867918831",
+  api_secret: "f6eDjwImqYHPSfprI93HS7B3R4I",
 });
-console.log("Cloudinary URL:", process.env.CLOUDINARY_URL); // Check if it's correctly loaded
+console.log("Cloudinary URL:", process.env.CLOUDINARY_URL);
 
 module.exports = cloudinary;
